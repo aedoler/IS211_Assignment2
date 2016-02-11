62b5e13b7a32ccccc04787f1e1878cc8b648a5ac
[Alex] <aedoler@gmail.com>
2016-02-11 00:05:22 -0300

    update

--- assignment2.py
+++ assignment2.py
@@ -17,6 +17,7 @@ handler = logging.FileHandler(LOG_FILENAME)
 handler.setLevel(logging.ERROR)
 logger.addHandler(handler)
 
+
 def downloadData(url):
     """fetches info from a url.
     Args:
@@ -25,6 +26,7 @@ def downloadData(url):
         fetches website info
     Example:
         downloadData(www.facebook.com)
+        :rtype: variable
     """
     response = urllib2.urlopen(str(url))
 
@@ -41,35 +43,42 @@ def processData(csvData):
     datadict = {}
 
     for linenumber, row in enumerate(cr):
-        cust_id = row[0] #Assuming ID is first on spreadsheet, followed by name, b-day
+        cust_id = row[0]  # Assuming ID is first on spreadsheet, followed by name, b-day
         name = row[1]
         try:
             bday = datetime.datetime.strptime(row[2], format)
-        except TypeError:
+        except ValueError:
             logger.error('Error processing line {} for ID {}'.format(linenumber, cust_id))
 
         datadict[cust_id] = (name, bday)
 
     return datadict
 
+
 def displayPerson(id, personData):
+    """Returns person' data based on input ID
+    """
 
     try:
         personData.get(id, default=None)
     except:
         print "No user found with that ID"
     message = "Person{} is {} with a birthday " \
-           "of {}".format(id, personData[id][0], personData[id][1])
+              "of {}".format(id, personData[id][0], personData[id][1])
     return message
 
+
 def main():
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-url', type=str)
-        args = parser.parse_args()
-    except Exception:
-        sys.exit()
-    csvData = downloadData(args)
+    """Allows for user input in command line with parser.
+    :rtype: object
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-url', type=str)
+    args = parser.parse_args()
+    urlinput = args.url
+
+    # sys.exit()
+    csvData = downloadData(urlinput)
     personData = processData(csvData)
 
     userinput = int(raw_input('Please input an ID: '))
@@ -78,10 +87,6 @@ def main():
     else:
         displayPerson(userinput, personData)
 
-    return personData
 
 if __name__ == '__main__':
     main()
-
-
-﻿print processData('https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv')
